ddafb33944dc3359e2f105d1d444667aaf7ba6be
Michelle <michho@uw.edu>
2017-10-27 11:17:20 -0700

    edited app.spec.js to include test invertCase function

--- __tests__/app.spec.js
+++ __tests__/app.spec.js
@@ -3,4 +3,22 @@
 import {greet, invertCase} from '../js/app'; //import the functions to test!
 
 /* tests go here */
+describe('Basic math', function() {
+    test('should add numbers correctly', function() {
+        //javascript
+        let result = 1+1;
 
+        // assertion
+        expect(result).toEqual(2);
+    })
+})
+
+describe('invertCase() function', function() {
+    test('letter case should be interveted', function() {
+        let test = "my NAME is Michelle";
+        let expectedResult = "MY name IS mICHELLE";
+        let result = invertCase(test);
+
+        expect(expectedResult).toEqual(result);
+    })
+})
